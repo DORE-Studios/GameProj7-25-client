2da2e7a14315f5648cbcc45719b0779da9b34455
Biscuit779 <kaelan.mcdowall@gmail.com>
2025-07-04 19:02:09 +1200

    done more work. shut up zach. leave me alone type shit

--- src/Ship.java
+++ src/Ship.java
@@ -1,86 +1,72 @@
 import java.util.*;
 
-public class Ship
-{
-    public enum ShipType
-    {
+public class Ship{
+    private final record ShipStats(String name, int armor, int evasion, int initialFuel,
+        int fuelConsumption, int crewCapacity, int fuelTankCapacity, int shieldCapacity,
+        int maxHealth, int foodConsumption, double storageCapacity){}
+
+    public enum ShipType{
         RECON, CARGO, COMBAT, BIO, ROBOT
     }
 
     // Ship stats as records
-    private static final java.util.Map<ShipType, ShipStats> SHIP_STATS = new HashMap<>();
-    {
-        SHIP_STATS.put(ShipType.RECON, new ShipStats("Recon", 0, 40, 000, 75, 4, 900, 600, 800, 0, 0.0));
-        SHIP_STATS.put(ShipType.CARGO, new ShipStats("Combat", 0, 30, 000, 100, 6, 1200, 800, 1200, 0, 0.0));
-        SHIP_STATS.put(ShipType.COMBAT, new ShipStats("Cargo", 0, 20, 000, 125, 8, 1500, 1200, 1800, 0, 0.0));
+    private static final java.util.Map<ShipType, ShipStats> SHIP_STATS = new HashMap<>();{
+        SHIP_STATS.put(ShipType.RECON, new ShipStats("Recon", 0, 40, 750, 75, 4, 900, 600, 800, 0, 0.0));
+        SHIP_STATS.put(ShipType.CARGO, new ShipStats("Combat", 0, 30, 800, 100, 6, 1200, 800, 1200, 0, 0.0));
+        SHIP_STATS.put(ShipType.COMBAT, new ShipStats("Cargo", 0, 20, 1200, 125, 8, 1500, 1200, 1800, 0, 0.0));
         SHIP_STATS.put(ShipType.BIO, new ShipStats("Bio", 0, 30, 000, 0, 0, 0, 0, 3000, 0, 0.0));
-        SHIP_STATS.put(ShipType.ROBOT, new ShipStats("Robot", 0, 30, 000, 150, 0, 2100, 1600, 1600, 0, 0.0));
+        SHIP_STATS.put(ShipType.ROBOT, new ShipStats("Robot", 0, 30, 1600, 150, 0, 2100, 1600, 1600, 0, 0.0));
     }
 
     //the variables:
-    private int crew; //
-    private double storage; //tbd
-    private int fuel_tank; //
+    private ShipType thisShipType;
+    private int crew;          //
+    private double storage;    //tbd
+    private int fuel_tank;     //
     private int shield_health; //
-    private int health; //
-    private int currency; //
-    private int x = 0, y = 0; //position >:-)
+    private int health;        //
+    private int currency;      //
+    private int x = 0, y = 0;  //position >:-)
 
-    public Ship(ShipType shipType)
-    {
+    public Ship(ShipType shipType){
+        thisShipType = shipType;
         ShipStats baseStats = SHIP_STATS.get(shipType);
 
         this.crew = baseStats.crewCapacity == 0 ? 0 : 2;
-        this.storage = 0; //tbd
+        this.storage = 0;                                   //tbd
         this.fuel_tank = baseStats.initialFuel();
         this.shield_health = baseStats.shieldCapacity();
         this.health = baseStats.maxHealth();
-
-        // switch (ship_type)
-        // {
-        // case "Recon": //#0
-        //     this.ship_num = 0;
-        //     this.crew = 2;
-        //     this.storage = 0; //tbd
-        //     this.fuel_tank = 750;
-        //     this.shield_health = this.stats[0][5];
-        //     this.health = this.stats[0][6];
-        //     break;
-        // case "Combat": //#1
-        //     this.ship_num = 1;
-        //     this.crew = 2;
-        //     this.storage = 0; //tbd
-        //     this.fuel_tank = 800;
-        //     this.shield_health = this.stats[1][5];
-        //     this.health = this.stats[1][6];
-        //     break;
-        // case "Cargo": //#2
-        //     this.ship_num = 2;
-        //     this.crew = 2;
-        //     this.storage = 0; //tbd
-        //     this.fuel_tank = 1000;
-        //     this.shield_health = this.stats[2][5];
-        //     this.health = this.stats[2][6];
-        //     break;
-        // case "Bio": //#3
-        //     this.ship_num = 3;
-        //     this.crew = 0; //no crew
-        //     this.storage = 0; //tbd
-        //     this.fuel_tank = 0; //no fuel
-        //     this.shield_health = this.stats[3][5];
-        //     this.health = this.stats[3][6];
-        //     break;
-        // case "Robot": //#4
-        //     this.ship_num = 4;
-        //     this.crew = 0; //no crew
-        //     this.storage = 0; //tbd
-        //     this.fuel_tank = 1200;
-        //     this.shield_health = this.stats[4][5];
-        //     this.health = this.stats[4][6];
-        //     break;
-        // }
     }
 
-    private final record ShipStats(String name, int armor, int evasion, int initialFuel, int fuelConsumption, int crewCapacity, int fuelTankCapacity, int shieldCapacity, int maxHealth, int foodConsumption, double storageCapacity)
-    {}
+    public void shootAt(Ship S, Weapon W){
+        S.shotAt(W);
+    }
+
+    public void shotAt(Weapon W){
+        W.attack(this);
+    }
+
+    public int getEvasion(){
+        return SHIP_STATS.get(thisShipType).evasion();
+    }
+
+    public int getShield(){
+        return this.shield_health;
+    }
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public void reduceShield(int damage){
+        this.shield_health -= damage;
+        if(this.shield_health < 0){
+            this.shield_health = 0;
+        }
+    }
+
+    public void reduceHealth(int damage){
+        this.health -= damage;
+    }
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -1,11 +1,13 @@
 public class Weapon{
     protected int accuracy;
+    protected boolean shieldEffective;
 
-    public Weapon(int accuracy){
+    public Weapon(int accuracy, boolean bool){
         this.accuracy = accuracy;
+        this.shieldEffective = bool;
     }
 
-    public int does_hit(int evasion){
+    protected int does_hit(int evasion){
         int chance = (int)(Math.random()*100)+1;
         if(accuracy > evasion){
             return 2;
@@ -16,8 +18,22 @@ public class Weapon{
         }
     }
 
-    public int roll_dmg(int min, int max){
+    protected int roll_dmg(int min, int max){
         return (int)(Math.random()*(max-min+1))+min;
+    }
+
+    public void attack(Ship S){}
+
+    protected void doDamage(Ship S, int dmg){
+        if(S.getShield() > 0){
+            if(shieldEffective){
+                S.reduceShield(dmg*2);
+            }else{
+                S.reduceShield(dmg);
+            }
+        }else{
+            S.reduceHealth(dmg);
+        }
     }
 }
 
@@ -34,49 +50,50 @@ class Minigun extends Weapon{
     int shots = 10, dmg = 0;
 
     public Minigun(){
-        super(20);
+        super(20, false);
     }
     
-    public int attack(int evasion){
-        if(does_hit(evasion) == 2){
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) == 2){
             for(int i = 0; i < shots; i++){
-                dmg += roll_dmg(25, 40);
+                dmg = roll_dmg(25, 40);
+                doDamage(S, dmg);
             }
         }else{
             for(int i = 0; i < shots; i++){
-                if(does_hit(evasion) == 1){
-                    dmg += roll_dmg(25, 40);
+                if(does_hit(S.getEvasion()) == 1){
+                    dmg = roll_dmg(25, 40);
+                    doDamage(S, dmg);
                 }
             }
         }
-        return dmg;
     }
 }
 
 class Railgun extends Weapon{
+    int dmg = 0;
     public Railgun(){
-        super(50);
+        super(50, false);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) < 0){
-            return roll_dmg(250, 350);
-        }else{
-            return 0;
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) < 0){
+            dmg = roll_dmg(250, 350);
+            doDamage(S, dmg);
         }
     }
 }
 
 class Sniper_Rifle extends Weapon{
+    int dmg;
     public Sniper_Rifle(){
-        super(40);
+        super(40, false);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) < 0){
-            return roll_dmg(200, 300);
-        }else{
-            return 0;
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) < 0){
+            dmg = roll_dmg(200, 300);
+            doDamage(S, dmg);
         }
     }
 }
@@ -85,22 +102,23 @@ class Chaingun extends Weapon{
     int shots = 15, dmg = 0;
 
     public Chaingun(){
-        super(25);
+        super(25, false);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) == 2){
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) == 2){
             for(int i = 0; i < shots; i++){
-                dmg += roll_dmg(30, 45);
+                dmg = roll_dmg(30, 45);
+                doDamage(S, dmg);
             }
         }else{
             for(int i = 0; i < shots; i++){
-                if(does_hit(evasion) == 1){
-                    dmg += roll_dmg(30, 45);
+                if(does_hit(S.getEvasion()) == 1){
+                    dmg = roll_dmg(35, 45);
+                    doDamage(S, dmg);
                 }
             }
         }
-        return dmg;
     }
 }
 
@@ -108,20 +126,23 @@ class Flak_Cannon extends Weapon{
     int shots = 50, dmg = 0;
     
     public Flak_Cannon(){
-        super(20);
+        super(20, false);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) == 2){
-            dmg += 4*shots;
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) == 2){
+            for(int i = 0; i < shots; i++){
+                dmg = roll_dmg(4, 8);
+                doDamage(S, dmg);
+            }
         }else{
             for(int i = 0; i < shots; i++){
-                if(does_hit(evasion) == 1){
-                    dmg += 4;
+                if(does_hit(S.getEvasion()) == 1){
+                    dmg = roll_dmg(4, 8);
+                    doDamage(S, dmg);
                 }
             }
         }
-        return dmg;
     }
 }
 
@@ -129,35 +150,36 @@ class Laser_Beam extends Weapon{
     int shots = 20, dmg = 0;
 
     public Laser_Beam(){
-        super(40);
+        super(40, true);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) == 2){
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) == 2){
             for(int i = 0; i < shots; i++){
-                dmg += roll_dmg(10, 15);
+                dmg = roll_dmg(10, 15);
+                doDamage(S, dmg);
             }
         }else{
             for(int i = 0; i < shots; i++){
-                if(does_hit(evasion) == 1){
-                    dmg += roll_dmg(10, 15);
+                if(does_hit(S.getEvasion()) == 1){
+                    dmg = roll_dmg(10, 15);
+                    doDamage(S, dmg);
                 }
             }
         }
-        return dmg;
     }
 }
 
 class Arc_Launcher extends Weapon{
+    int dmg;
     public Arc_Launcher(){
-        super(60);
+        super(60, true);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) < 0){
-            return 300;
-        }else{
-            return 0;
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) < 0){
+            dmg = 300;
+            doDamage(S, accuracy);
         }
     }
 }
@@ -166,91 +188,107 @@ class Plasma_Blast extends Weapon{
     int shots = 10, dmg = 0;
 
     public Plasma_Blast(){
-        super(20);
+        super(20, true);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) == 2){
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) == 2){
             for(int i = 0; i < shots; i++){
-                dmg += roll_dmg(20, 30);
+                dmg = roll_dmg(20, 30);
+                doDamage(S, dmg);
             }
         }else{
             for(int i = 0; i < shots; i++){
-                if(does_hit(evasion) == 1){
-                    dmg += roll_dmg(20, 30);
+                if(does_hit(S.getEvasion()) == 1){
+                    dmg = roll_dmg(20, 30);
+                    doDamage(S, dmg);
                 }
             }
         }
-        return dmg;
     }
 }
 
 class Microwave_Ray extends Weapon{
+    int shots = 40, dmg = 0;
     public Microwave_Ray(){
-        super(20);
+        super(20, true);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) < 0){
-            return 8;
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) == 2){
+            for(int i = 0; i < shots; i++){
+                dmg = 6;
+                doDamage(S, dmg);
+            }
         }else{
-            return 0;
+            for(int i = 0; i < shots; i++){
+                if(does_hit(S.getEvasion()) == 1){
+                    dmg = 6;
+                    doDamage(S, dmg);
+                }
+            }
         }
     }
 }
 
 class Hard_Light_Cannon extends Weapon{
     public Hard_Light_Cannon(){
-        super(0);               //tbd
+        super(0, true);               //tbd
     }
 }
 
 class EMP extends Weapon{
     public EMP(){
-        super(100);             //uhhh ill do that later
+        super(100, false);             //uhhh ill do that later
     }
 }
 
 class BFG extends Weapon{
+    int dmg;
     public BFG(){
-        super(100);
+        super(100, false);
     }
 
-    public int attack(int evasion){
-        return roll_dmg(1200, 1500);
+    public void attack(Ship S){
+        dmg = roll_dmg(1200, 1500);
+        doDamage(S, dmg);
     }
 }
 
 class Missle extends Weapon{
+    int dmg;
     public Missle(){
-        super(100);
+        super(100, false);
     }
 
-    public int attack(int evasion){
-        return 100;
+    public void attack(Ship S){
+        dmg = 100;
+        doDamage(S, dmg);
     }
 }
 
 class Torpedo extends Weapon{
+    int dmg;
     public Torpedo(){
-        super(65);
+        super(65, false);
     }
 
-    public int attack(int evasion){
-        if(does_hit(evasion) < 0){
-            return 200;
-        }else{
-            return 0;
+    public void attack(Ship S){
+        if(does_hit(S.getEvasion()) < 0){
+            dmg = 200;
+            doDamage(S, dmg);
         }
     }
 }
 
 class Nuke extends Weapon{
+    int dmg;
     public Nuke(){
-        super(100);
+        super(100, false);
     }
 
-    public int attack(int evasion){
-        return 500;
+    public void attack(Ship S){
+        dmg = 500;
+        doDamage(S, dmg);
     }
 }
